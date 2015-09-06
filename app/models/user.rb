4703e19b21502b4950d538d8d3830a14ca50a116
Alloffices <alloffices.nyc@gmail.com>
2015-09-06 00:56:37 -0400

    add user signup AdminMailer notification

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,10 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
   has_many :projects
 
+  after_create :send_notification
+
+  def send_notification
+  	AdminMailer.new_user(self).deliver_now
+  end
+
 end
